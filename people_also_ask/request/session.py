84c408179b2794330220aa9b89ba0b1ce9d8edba
Daaviid37 <58936086+Daaviid37@users.noreply.github.com>
2023-10-20 09:52:09 +0200

    Update session.py

--- people_also_ask/request/session.py
+++ people_also_ask/request/session.py
@@ -2,6 +2,8 @@ import os
 import logging
 import requests
 import traceback
+import random
+import time
 
 from people_also_ask.tools import retryable
 from itertools import cycle
@@ -11,30 +13,46 @@ from people_also_ask.exceptions import RequestError
 
 from requests import Session as _Session
 
+# 1. Importaciones adicionales
+from random import choice
 
 SESSION = _Session()
-NB_TIMES_RETRY = os.environ.get(
-    "RELATED_QUESTION_NB_TIMES_RETRY", 3
-)
-NB_REQUESTS_LIMIT = os.environ.get(
-    "RELATED_QUESTION_NB_REQUESTS_LIMIT", 25
-)
-NB_REQUESTS_DURATION_LIMIT = os.environ.get(
-    "RELATED_QUESTION_NB_REQUESTS_DURATION_LIMIT", 60  # seconds
-)
-logging.basicConfig()
-semaphore = CallingSemaphore(
-    NB_REQUESTS_LIMIT, NB_REQUESTS_DURATION_LIMIT
-)
-HEADERS = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-    " AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/84.0.4147.135 Safari/537.36"
-}
+NB_TIMES_RETRY = os.environ.get("RELATED_QUESTION_NB_TIMES_RETRY", 3)
+NB_REQUESTS_LIMIT = os.environ.get("RELATED_QUESTION_NB_REQUESTS_LIMIT", 25)
+NB_REQUESTS_DURATION_LIMIT = os.environ.get("RELATED_QUESTION_NB_REQUESTS_DURATION_LIMIT", 60)  # seconds
 
+logging.basicConfig()
+
+# 2. Lista de cabezales de navegador (User-Agents)
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
+    "Mozilla/5.0 (Windows NT 10.0; Trident/7.0; AS; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0",
+]
+
+
+# Función para obtener un cabezal de navegador aleatorio
+def get_random_user_agent():
+    return choice(USER_AGENTS)
 
 logger = logging.getLogger(__name__)
-
 
 class ProxyGeneator:
 
@@ -77,18 +95,20 @@ def set_proxies(proxies: Optional[tuple]) -> ProxyGeneator:
 
 set_proxies(proxies=_load_proxies())
 
-
 @retryable(NB_TIMES_RETRY)
 def get(url: str, params) -> requests.Response:
     proxies = PROXY_GENERATORS.get()
     try:
         with semaphore:
+            # 3. Modificación para usar el cabezal aleatorio
             response = SESSION.get(
                 url,
                 params=params,
-                headers=HEADERS,
+                headers={"User-Agent": get_random_user_agent()},
                 proxies=proxies,
             )
+            # Añadir un tiempo de espera aleatorio entre peticiones
+            time.sleep(random.uniform(10, 45))
     except Exception:
         raise RequestError(
             url, params, proxies, traceback.format_exc()
