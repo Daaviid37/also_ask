e2da9d42cb8cc83475b8499103ebf8c03e29df49
Daaviid37 <58936086+Daaviid37@users.noreply.github.com>
2023-10-20 11:41:09 +0200

    Update session.py

--- people_also_ask/request/session.py
+++ people_also_ask/request/session.py
@@ -2,29 +2,39 @@ import os
 import logging
 import requests
 import traceback
-import time
+
 from people_also_ask.tools import retryable
+from itertools import cycle
 from typing import Optional
-from threading import Semaphore
+from people_also_ask.tools import CallingSemaphore
 from people_also_ask.exceptions import RequestError
 
 from requests import Session as _Session
 
+
+SESSION = _Session()
+NB_TIMES_RETRY = os.environ.get(
+    "RELATED_QUESTION_NB_TIMES_RETRY", 3
+)
+NB_REQUESTS_LIMIT = os.environ.get(
+    "RELATED_QUESTION_NB_REQUESTS_LIMIT", 25
+)
+NB_REQUESTS_DURATION_LIMIT = os.environ.get(
+    "RELATED_QUESTION_NB_REQUESTS_DURATION_LIMIT", 60  # seconds
+)
+logging.basicConfig()
+semaphore = CallingSemaphore(
+    NB_REQUESTS_LIMIT, NB_REQUESTS_DURATION_LIMIT
+)
 HEADERS = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     " AppleWebKit/537.36 (KHTML, like Gecko) "
     "Chrome/84.0.4147.135 Safari/537.36"
 }
 
-SESSION = _Session()
-NB_TIMES_RETRY = os.environ.get("RELATED_QUESTION_NB_TIMES_RETRY", 3)
-NB_REQUESTS_LIMIT = os.environ.get("RELATED_QUESTION_NB_REQUESTS_LIMIT", 25)
-NB_REQUESTS_DURATION_LIMIT = os.environ.get("RELATED_QUESTION_NB_REQUESTS_DURATION_LIMIT", 60)  # seconds
-
-logging.basicConfig()
-semaphore = Semaphore(1)
 
 logger = logging.getLogger(__name__)
+
 
 class ProxyGeneator:
 
@@ -49,20 +59,24 @@ class ProxyGeneator:
             "https": proxy
         }
 
+
 def _load_proxies() -> Optional[tuple]:
     filepath = os.getenv("PAA_PROXY_FILE")
     if filepath:
-        with open(filepath, "r") as fd:
+        with open(filepath, "w") as fd:
             proxies = [e.strip() for e in fd.read().splitlines() if e.strip()]
     else:
         proxies = None
     return proxies
 
+
 def set_proxies(proxies: Optional[tuple]) -> ProxyGeneator:
     global PROXY_GENERATORS
     PROXY_GENERATORS = ProxyGeneator(proxies=proxies)
 
+
 set_proxies(proxies=_load_proxies())
+
 
 @retryable(NB_TIMES_RETRY)
 def get(url: str, params) -> requests.Response:
