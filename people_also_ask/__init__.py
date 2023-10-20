fb847e8edc01310391752abba04da621922d3c25
Daaviid37 <58936086+Daaviid37@users.noreply.github.com>
2023-10-20 10:14:19 +0200

    Update __init__.py

--- people_also_ask/__init__.py
+++ people_also_ask/__init__.py
@@ -1,12 +1,3 @@
 #! /usr/bin/env python3
 from .request.session import set_headers
-from .google import get_related_questions, get_answer
-
-
-from people_also_ask.google import (
-    get_answer,
-    generate_answer,
-    get_simple_answer,
-    get_related_questions,
-    generate_related_questions,
-)
+from .google import get_related_questions, get_answer, generate_answer, get_simple_answer, generate_related_questions
